a90fcfb8af92c93c5ee678bd2f17aa744b71f608
imvickykumar999 <imvickykumar999@gmail.com>
2022-05-28 01:08:29 +0530

    waiting for approval

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
+
 import razorpay
 import json
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__,static_folder = "static", static_url_path='')
-razorpay_client = razorpay.Client(auth=("<APP_ID>", "<APP_SECRET>"))
-
+razorpay_client = razorpay.Client(auth=("***********", "****************"))
 
 @app.route('/')
 def app_create():
     return render_template('app.html')
 
-
 @app.route('/charge', methods=['POST'])
 def app_charge():
-    amount = 5100
+    amount = 200
     payment_id = request.form['razorpay_payment_id']
     razorpay_client.payment.capture(payment_id, amount)
     return json.dumps(razorpay_client.payment.fetch(payment_id))
